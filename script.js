147c7e95bfe21fc2692a7378cdbdaa7ad32ff733
vyshak-sukumaran <vyshakvyshu791@gmail.com>
2022-11-11 17:04:32 +0530

    album horizontal

--- script.js
+++ script.js
@@ -34,25 +34,4 @@ album.addEventListener("wheel", (e) => {
   })
 })
 
-// zoom and pan
-
-const elem = document.querySelector(".tree-container")
-const panzoom = Panzoom(elem, {
-  maxScale: 6,
-  startScale: 0.6
-})
-
-document.querySelector(".tree").addEventListener("wheel", panzoom.zoomWithWheel);
-//   document.querySelector(".zoom-area img").addEventListener("click", function() {
-//     alert("clicked!");
-//   });
-
-// scroll to me
-
-const me = document.querySelector('#me')
-window.onload = () => {
-  me && me.scrollIntoView()
-}
-
-
 
