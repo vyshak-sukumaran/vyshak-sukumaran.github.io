554aa78fe24c5af7a5c2831d6437f30e610b3ef4
vyshak-sukumaran <vyshakvyshu791@gmail.com>
2022-11-11 11:37:22 +0530

    tree

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const elem = document.querySelector(".tree-container")
 const panzoom = Panzoom(elem, {
   maxScale: 6
 })
+panzoom.pan(10, 10)
+panzoom.zoom(0.5, { animate: true })
 
 document.querySelector(".tree").addEventListener("wheel", panzoom.zoomWithWheel);
 //   document.querySelector(".zoom-area img").addEventListener("click", function() {
